a40313eafe5f3c4cf99e0ec459d8bf17034eeb5d
sergeyghazaryan2011 <109455278+sergeyghazaryan2011@users.noreply.github.com>
2022-08-18 14:38:39 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+
+
+
+if(true){
+
+
+
+
+//good game
+
+//math test
+function gamestart(){
+    
+    let a = +prompt("2000+10000");
+    alert(a)
+    if(a == 12000){
+        alert(true);
+        let b = +prompt('15000 * 4');
+        alert(b)
+        if(b == 60000){
+            alert(true);
+            let c = prompt('6000-1500');
+            alert(c);
+            if(c == 4500){
+                alert(true);
+                let d = prompt('125*8');
+                alert(d)
+                if(d == 1000){
+                    alert(true)
+                    let e = prompt('200^2');
+                    alert(e);
+                    if(e == 40000){
+                        alert(true)
+                        let f = prompt('150+45-20');
+                        alert(f);
+                        if(f == 95){
+                            alert(true);
+                            let g = prompt('24+12+46');
+                            alert(g)
+                            if(g == 90){
+                                alert(true);
+                                let h = prompt('15+49+11')
+                                alert(h);
+                                if(h == 75){
+                                    alert('true')
+                                    let a1 = prompt('x = 20 * 12 * 0')
+                                    alert(a1);
+                                    if(a1 == 0){
+                                        alert(true)
+                                        let a2 = prompt('25*48*12-150-152+85+55*0+5');
+                                        alert(a2);
+                                        if(a2 == 5){
+                                            alert(true)
+                                            let a3 = prompt('12+48+15+30+95')
+                                            alert(a3)
+                                            if(a3 == 200){
+                                                alert(true)
+                                                let a4 = prompt('2022-2013*5')
+                                                alert(a4)
+                                                if(a4 == 45){
+                                                   alert(true)
+                                                   alert('win')
+                                                }else{
+                                                    alert(false)
+                                                }
+                                            }else{
+                                                alert(false)
+                                            }
+                                        }else{
+                                            alert(false)
+                                        }
+                                    }else{
+                                        alert(false)
+                                    }
+                                }else{
+                                    alert(false)
+                                }
+                            }else{
+                                alert(false);
+                            }
+                        }else{
+                            alert(false)
+                        }
+                    }else{
+                        alert(false)
+                    }
+                }else{
+                    alert(false)
+                }
+            }else{
+                alert(false);
+            }
+        }else{
+            alert(false);
+        }
+    }else{
+        alert(false);
+    }
+}
+}
